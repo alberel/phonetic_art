4bdf9e2c60556be924b3515a70da21449376a785
alberel <auron_ff10@hotmail.co.uk>
2016-06-09 12:02:00 +0100

    Put sketch in a class

--- static/js/workings.js
+++ static/js/workings.js
@@ -1,16 +1,20 @@
+var sketch = function(p){
+	p.setup = function(){
+		myCanvas = createCanvas(getElement('myContainer').width, 1000);
+		myCanvas.parent('myContainer');
+		stroke(0);
+	}
+
+	p.draw = function(){
+		background(0,0,0);
+	}
+}
+
+var myp5 = new p5(sketch);
+
 var submitButton;
 
-function setup() {
-	myCanvas = createCanvas(getElement('myContainer').width, 1000);
-	myCanvas.parent('myContainer');
-	stroke(0);
-}
 
-function draw()
-{
-	background(0,0,0);
-  
-}
 
 function transcribeText(phrase)
 {
